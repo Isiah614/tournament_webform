855d7b886b4f55848c8da06c107c7ea6a2b3885e
Isiah Harris <percy.isiah.harris@gmail.com>
2021-03-13 19:34:39 -0500

    completed forms with updated nuget packages

--- tournament_tracker_revised/tournament_tracker_revised/TrackerUI/Forms/CreateTournamentForm.Designer.cs
+++ tournament_tracker_revised/tournament_tracker_revised/TrackerUI/Forms/CreateTournamentForm.Designer.cs
@@ -222,6 +222,7 @@ namespace TrackerUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(919, 597);
             this.Controls.Add(this.createTournamentBuildingButton);
             this.Controls.Add(this.prizesDeleteSelectedButton);
