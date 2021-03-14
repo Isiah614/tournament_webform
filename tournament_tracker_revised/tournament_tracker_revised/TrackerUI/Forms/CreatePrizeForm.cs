62021451957c160bdef1d3b11a4d09edfa4bf03b
Isiah Harris <percy.isiah.harris@gmail.com>
2021-03-14 00:11:12 -0500

    pushing up midway through Create Prize Form completion

--- tournament_tracker_revised/tournament_tracker_revised/TrackerUI/Forms/CreatePrizeForm.cs
+++ tournament_tracker_revised/tournament_tracker_revised/TrackerUI/Forms/CreatePrizeForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using TrackerLibrary.Models;
 
 namespace TrackerUI.Forms
 {
@@ -35,7 +36,15 @@ namespace TrackerUI.Forms
 
         private void createPrizeButton_Click(object sender, EventArgs e)
         {
+            if(ValidateForm())
+            {
+                PrizeModel model = new PrizeModel(
+                    placeNumberTextBox.Text, 
+                    placeNumberTextBox.Text, 
+                    prizeAmountTextBox.Text, 
+                    prizeAmountTextBox.Text);
 
+            }
         }
 
         private bool ValidateForm()
@@ -45,6 +54,35 @@ namespace TrackerUI.Forms
             bool placeNumberValidNumber = int.TryParse(placeNumberTextBox.Text, out placeNumber);
 
             if (placeNumberValidNumber == false)
+            {
+                output = false;
+            }
+            if (placeNumber < 1)
+            {
+                output = false;
+            }
+            if (placeNameTextBox.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            decimal prizeAmout = 0;
+            int prizePercentage = 0;
+
+            bool prizeAmountValid = decimal.TryParse(prizeAmountTextBox.Text, out prizeAmout);
+            bool prizePercentageValid = int.TryParse(prizePercentageTextBox.Text, out prizePercentage);
+
+            if(prizeAmountValid == false || prizePercentageValid == false)
+            {
+                output = false;
+            }
+
+            if(prizeAmout <= 0 && prizePercentage <= 0)
+            {
+                output = false;
+            }
+
+            if (prizePercentage < 0 || prizePercentage > 100)
             {
                 output = false;
             }
